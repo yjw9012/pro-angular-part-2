0f523848a52d7a066b6329fdf60a722db4e136b5
Jiwoong Youn <yjw9012job@gmail.com>
2016-04-14 00:09:26 -0700

    Chapter 13: Understanding data inheritance
    Worth reviewing

--- controllers.js
+++ controllers.js
@@ -10,3 +10,43 @@ controllersModule.controller("dayCtrl", function ($scope, days) {
 controllersModule.controller("tomorrowCtrl", function ($scope, days) {
     $scope.day = days.tomorrow;
 });
+
+
+// Chapter 13
+const app = angular.module("exampleApp", []);
+app.controller("topLevelCtrl", function ($scope) {
+    $scope.dataValue = "Hello, Adam";
+
+    // Solution
+    $scope.data = {
+        dataValue : "Hello, Jiwoong"
+    };
+
+    $scope.reverseText = function () {
+        $scope.dataValue = $scope.dataValue.split("").reverse().join("");
+    };
+    $scope.changeCase = function () {
+        var result = [];
+        angular.forEach($scope.dataValue.split(""), function (char, index) {
+            result.push(index % 2 == 1 ? char.toString().toUpperCase() : char.toString().toLowerCase());
+        });
+        $scope.dataValue = result.join("");
+    };
+});
+app.controller("firstChildCtrl", function ($scope) {
+    $scope.changeCase = function () {
+        $scope.dataValue = $scope.dataValue.toUpperCase();
+    };
+});
+app.controller("secondChildCtrl", function ($scope) {
+    $scope.changeCase = function () {
+        $scope.dataValue = $scope.dataValue.toLowerCase();
+    };
+    $scope.shiftFour = function () {
+        var result = [];
+        angular.forEach($scope.dataValue.split(""), function (char, index) {
+            result.push(index < 4 ? char.toUpperCase() : char);
+        });
+        $scope.dataValue = result.join("");
+    };
+});
